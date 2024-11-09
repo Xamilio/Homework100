445900df2b5df3e5f051506926fe686f2011eda1
Xamilio <Xamilio25@gmail.com>
2024-11-09 18:57:46 +0300

    Добавьте файлы проекта.

---
+++ Homework100/Homework100.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int const N = 8;
+    int arr[N] = { 54, 42, 39, 37, 12, 5, 3, 1 };
+    int left = 0;
+    int right = N - 1;
+    int middleIndex;
+    int search;
+
+    cout << "Enter search: ";
+    cin >> search;
+
+    int index = -1;
+
+    while (left <= right)
+    {
+        middleIndex = (left + right) / 2;
+
+        if (arr[middleIndex] == search)
+        {
+            index = middleIndex;
+            break;
+        }
+        else if (arr[middleIndex] > search)
+        {
+            left = middleIndex + 1;
+        }
+        else
+        {
+            right = middleIndex - 1;
+        }
+    }
+
+    cout << index << endl;
+
+}
